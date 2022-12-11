0e59c21ddd213fe0a58aa90093dc148dbe8fae63
rosmarmendoza <rosmar.mendoza@wearemo.com>
2022-12-11 18:28:13 -0500

    mi primer commit

---
+++ src/main/java/com/dh/demoClinicaOdontologicaRM/DemoClinicaOdontologicaRmApplication.java
@@ -0,0 +1,13 @@
+package com.dh.demoClinicaOdontologicaRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoClinicaOdontologicaRmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoClinicaOdontologicaRmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dh/demoClinicaOdontologicaRM/DemoClinicaOdontologicaRmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.demoClinicaOdontologicaRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoClinicaOdontologicaRmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
